d2a3266106900f576bc8bd81f929bf1795bafed1
FelixLST <30856703+FelixLST@users.noreply.github.com>
2017-11-24 10:40:52 -0600

    Update PI Prototipo.cpp

--- PI Prototipo.cpp
+++ PI Prototipo.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 void Menu();
 int Moda(int NumPregun,int Contador[365]); //Regresa la moda de la preguntra dada
+int ModaImp(int NumPregun,int Contador[365]); //Regresa la moda de la preguntra dada
 int Contador(int NumPregun,int Contador[365]); //Cuenta las respuestra de la pregunta dada y da la frecuencia de cada una
 int FrecuenciaRelativaYAcum(int NumPregun,int Contador[365]); //Regresa la fecuencia relativa y acumulada de los resultados de la pregunta dada
-void LectorPrincipal(Char Respuestas[][73],int r); //Lee todo el archivo de exel-Felix
-void impresorExelDatos(int NumPregun,char IndiPregun[73], char Respuestas[][73]int r);// Crea un archivo con los datos del resumen-Felix
+int FrecuenciaRelativaYAcumImp(int NumPregun,int Contador[365]); //Regresa la fecuencia relativa y acumulada de los resultados de la pregunta dada
+void LectorPrincipal(Char Respuestas[][73],char IndiPregun[73], int r); //Lee todo el archivo de exel-Felix
+void ImpresorExelDatos(int NumPregun,char IndiPregun[73], char Respuestas[][73],int r);// Crea un archivo con los datos del resumen-Felix
 void LectorPregunta(int NumPregun,char RespuestaColum[365]);//Lee solo los datos de la preungta dada
 void ResumenGeneral(int Contador[365],Char IndiPregun[73], char Respuestas[][73],int r);//Imprime en pantalla los datos de todo el archivo (Todas las modas, todas las fecuencias,etc)
 
@@ -26,19 +28,19 @@ int main()
 		{
 			cout << "De que Pregunta desea saber la moda?\n";
 			cin >> NumPregun;
-			cout << "La moda de la pregunta " << NumPregun << " es " << Moda(NumPregun); << endl;
+			cout << "La moda de la pregunta " << NumPregun << " es " << Moda(NumPregun,Contador); << endl;
 		}
 		else if(op==2);
 		{
 			cout << "De que pregunta desea saber la frecuencia \n";
 			cin >> Numpregun;
-			cout << "Las frecuencias  de la pregunta " << Numpregun << " es " << Contador(NumPregun) << endl;
+			cout << "Las frecuencias  de la pregunta " << Numpregun << " es " << Contador(NumPregun,Contador); << endl;
 		}
 		else if(op==3);
 		{
 			cout << "De que pregunta desea saber la Frecuencia relativa y  acumulada\n";
 			cin >> Numpregun;
-			cout << "La fecuencia relativa acumulada de la pregunta " << Numpregun << "es" << FrecuenciaRelativaYAcum(Numpregun) << endl;
+			cout << "La fecuencia relativa acumulada de la pregunta " << Numpregun << "es" << FrecuenciaRelativaYAcum(NumPregun,Contador); << endl;
 		}
 		else if(op==4);
 		{
@@ -46,13 +48,13 @@ int main()
 			cin >> Numpregun;
 			if(Numpregun==1)
 			{
-				ResumenGeneral();
-				impresorExelDatos();
-				cout << "Se an guardado los resultados en un archivo de exel llamado *Resultado de encuenstas* en la carpeta Encuestas PI en el disco duro\n";
+				ResumenGeneral(Contador,IndiPregun,Respuestas,365);
+				ImpresorExelDatos(NumPregun,IndiPregun,Respuestas,365);
+				cout << "Se an guardado los resultados en un archivo de exel llamado *ResultadoDeEncuenstas* en la carpeta Encuestas PI en el disco duro\n";
 
 			}
 			else
-				ResumenGeneral();
+				ResumenGeneral(Contador,IndiPregun,Respuestas,365);
 
 		}
 		
@@ -70,12 +72,39 @@ void Menu();
 	cin >> op;
 	return op;
 }
-void impresorExelDatos(int NumPregun);//poner una oopcion para imprimir todo el archivo y no solo 1 pregunta.
+void ImpresorExelDatos(int NumPregun,char IndiPregun[73], char Respuestas[][73],int r,int Contador[365])//poner una oopcion para imprimir todo el archivo y no solo 1 pregunta.
 {
+	ifstream archi("c:\\datos\\ResultadoDeEncuenstas.xlsx",ios::out);
+		for(int i=0; i<73; i++)
+	{
+		arch<<IndiPregun[i];
+	}
+	for(int c=0; c<20; c++)
+	{
+		arch<<"Los resultados de la pregunta " << c << " Son" << endl;
+		arch<<"Moda" << endl;
+		ModaImp(c,Contador);
+		arch<<"Frecuencia relativa y acumulada "<<endl;
+		FrecuenciaRelativaYAcumImp(c,Contador);
+
+	}
+	
 
 }
-void LectorPrincipal();
+
+void LectorPrincipal(Char Respuestas[][73],char IndiPregun[73], int r)
 {
 	ifstream archi("c:\\datos\\PIBaseDeDatos.xlsx",ios::in);
+	for(int i=0; i<73; i++)
+	{
+		archi.getline()>>IndiPregun[i];
+	}
+	for(int c=0; c<73; c++)
+	{
+		for(int k=0; k<r; k++)
+		{
+			archi>>Respuestas[K][C];
+		}
+	}
 
 }
